9d0d936a813afbd101105f275e8526d22e36654c
Juan <juanprada538@gmail.com>
2026-06-19 23:05:37 -0500

    feat:add BD configuration and Docker composer for postgres DB

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { VehiclesModule } from './vehicles/vehicles.module';
+
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [VehiclesModule, ConfigModule.forRoot(),TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT ?? '5432', 10),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: 'gpsdb',
+      autoLoadEntities:true,
+      synchronize: true,
+    }),],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
